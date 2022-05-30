86b48eee9ca9b358f189cc4764d2f65ae881ab5a
jamie1077 <jm1077@sky.com>
2022-05-30 08:35:51 +0100

    book listing and new book form pages

---
+++ routes/books.js
@@ -0,0 +1,76 @@
+const express = require('express');
+const router = express.Router();
+const Book = require('../models').Book;
+
+/* Handler function to wrap each route. */
+function asyncHandler(cb){
+  return async(req, res, next) => {
+    try {
+      await cb(req, res, next)
+    } catch(error){
+      // Forward error to the global error handler
+      next(error);
+    }
+  }
+}
+
+/* GET all books listings */
+router.get('/', asyncHandler(async (req, res) => {
+  const books = await Book.findAll();
+  res.render("index", { books, title: "Books" });
+}));
+
+/* Create a new book. */
+router.get('/new', (req, res) => {
+  res.render("books/new", { book: {}, title: "New Book" });
+});
+
+/* POST create book. */
+router.post('/', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.create(req.body);
+    res.redirect("/books");
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") { // checking the error
+      book = await Book.build(req.body);
+      res.render("books/new", { book, errors: error.errors, title: "New Book" })
+    } else {
+      throw error; // error caught in the asyncHandler's catch block
+    }  
+  }
+}));
+
+/* GET individual book form page. */
+router.get('/:id', asyncHandler(async (req, res) => {
+  const book = await Book.findByPk(req.params.id);
+  if(book) {
+    res.render("books/update", { book, title: book.title });  
+  } else {
+    res.sendStatus(404);
+  }
+}));
+
+/* Update a book. */
+router.post('/:id/edit', asyncHandler(async (req, res) => {
+  let book;
+  try {
+    book = await Book.findByPk(req.params.id);
+    if(book) {
+      await book.update(req.body);
+      res.redirect("/books/" + book.id); 
+    } else {
+      res.sendStatus(404);
+    }
+  } catch (error) {
+    if(error.name === "SequelizeValidationError") {
+      book = await Book.build(req.body);
+      book.id = req.params.id; // make sure correct book gets updated
+      res.render("books/update", { book, errors: error.errors, title: "Update Book" })
+    } else {
+      throw error;
+    }
+  }
+}));
+
+module.exports = router;
